57320e65c8cb515287482c963b7ad45e882fb378
Jacob Ilias Komissar <jikomissar@wpi.edu>
2019-02-10 03:08:21 -0500

    Fix canvas aspect ratio at 5/3

--- main.js
+++ main.js
@@ -13,26 +13,30 @@ import * as MV from "./MV+.js";
 
 //// Prepare the canvas
 
-const MIN_CANVAS_HEIGHT = 200;
-const MIN_CANVAS_WIDTH  = 200;
-
 const X_FIELD_OF_VIEW = 90;
-const ASPECT_RATIO = 5/3; // canvas.width / canvas.height;
+const ASPECT_RATIO = 5/3;
+
+const MIN_CANVAS_HEIGHT = 200;
 
 const PERSPECTIVE_NEAR_PLANE = 0.001;
 const PERSPECTIVE_FAR_PLANE = 1000;
 
 const canvas = document.querySelector("#webglCanvas");
 
-// Resize the canvas
-canvas.width = Math.max(Math.round(document.body.clientWidth),
-                        MIN_CANVAS_WIDTH);
-{ // Set width to either 200 or as tall as it can be without pushing anything else off-screen
+{
+    // Set height to either 200 or as tall as it can be without pushing anything else off-screen
     canvas.height = 0;
     let body = window.getComputedStyle(document.body);
     let height = document.body.scrollHeight + parseInt(body.marginTop) + parseInt(body.marginTop);
 
     canvas.height = Math.max(MIN_CANVAS_HEIGHT, window.innerHeight - height);
+
+    // Set width to
+    canvas.width = (canvas.height * ASPECT_RATIO);
+    if (canvas.width > document.body.clientWidth) {
+        canvas.width = document.body.clientWidth;
+        canvas.height = canvas.width / ASPECT_RATIO;
+    }
 }
 
 
